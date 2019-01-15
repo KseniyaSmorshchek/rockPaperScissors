c622f5c36910a803319100fdf22d0c8c6ae29160
KseniyaSmorshchek <kseniya_smorshchek@epam.com>
2019-01-15 14:22:35 +0300

    Updates

---
+++ precache-manifest.8dab40bae57133c1deb8e62430cd5922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "021bd30f76af2cc388d7",
+    "url": "/rockPaperScissors/static/js/runtime~main.021bd30f.js"
+  },
+  {
+    "revision": "ed1ec1699cee7761d5ff",
+    "url": "/rockPaperScissors/static/js/main.ed1ec169.chunk.js"
+  },
+  {
+    "revision": "efff550f3f4283d8d000",
+    "url": "/rockPaperScissors/static/js/1.efff550f.chunk.js"
+  },
+  {
+    "revision": "ed1ec1699cee7761d5ff",
+    "url": "/rockPaperScissors/static/css/main.2147df73.chunk.css"
+  },
+  {
+    "revision": "efff550f3f4283d8d000",
+    "url": "/rockPaperScissors/static/css/1.8fae2b45.chunk.css"
+  },
+  {
+    "revision": "22d86d6947a44179c6aa7fc42c44f237",
+    "url": "/rockPaperScissors/index.html"
+  }
+];
